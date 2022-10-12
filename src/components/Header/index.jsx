3a649ddfb4539204c881c04d36accfe8af63d167
Sushant Garudkar <garudkar.sa@gmail.com>
2022-10-12 19:57:34 +0530

    Develop Order Card UI

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -6,10 +6,13 @@ import {
 } from '@heroicons/react/outline'
 import { signIn, signOut, useSession } from 'next-auth/react'
 import { useRouter } from 'next/router'
+import { useSelector } from 'react-redux'
+import { selectItems } from '../../slices/basketSlice'
 
 const Header = () => {
 	const { data: session } = useSession()
 	const router = useRouter()
+	const items = useSelector(selectItems)
 
 	return (
 		<header>
@@ -37,7 +40,10 @@ const Header = () => {
 
 				{/* Right */}
 				<div className='text-white flex items-center text-xs space-x-6 mx-6 whitespace-nowrap'>
-					<div onClick={!session ? signIn : signOut} className='link'>
+					<div
+						title={!session ? 'Sign In' : 'Sign Out'}
+						onClick={!session ? signIn : signOut}
+						className='link'>
 						<p>{session ? `Hello, ${session.user?.name}` : 'Sign In'}</p>
 						<p className='font-extrabold md:text-sm'>Account & Lists</p>
 					</div>
@@ -51,7 +57,7 @@ const Header = () => {
 						onClick={() => router.push('/checkout')}
 						className='relative link flex items-center'>
 						<span className='absolute top-0 right-0 md:right-10 h-4 w-4 bg-yellow-400 text-center rounded-full text-black font-bold'>
-							0
+							{items.length}
 						</span>
 						<ShoppingCartIcon className='h-10' />
 						<p className='hidden md:inline font-extrabold md:text-sm mt-2'>
--- src/components/Product/index.jsx
+++ src/components/Product/index.jsx
@@ -2,6 +2,8 @@ import Image from 'next/image'
 import { StarIcon } from '@heroicons/react/solid'
 import { useState } from 'react'
 import Currency from 'react-currency-formatter'
+import { useDispatch } from 'react-redux'
+import { addToBasket } from '../../slices/basketSlice'
 
 const Product = ({ id, title, price, description, category, image }) => {
 	const MAX_RATING = 5
@@ -11,6 +13,22 @@ const Product = ({ id, title, price, description, category, image }) => {
 		Math.floor(Math.random() * (MAX_RATING - MIN_RATING + 1)) + MIN_RATING
 	)
 	const [hasPrime] = useState(Math.random() < 0.5)
+	const dispatch = useDispatch()
+
+	const addItemToBasket = () => {
+		const product = {
+			id,
+			title,
+			price,
+			rating,
+			description,
+			category,
+			image,
+			hasPrime,
+		}
+
+		dispatch(addToBasket(product))
+	}
 
 	return (
 		<div className='relative flex flex-col m-5 bg-white z-30 p-10'>
@@ -43,7 +61,9 @@ const Product = ({ id, title, price, description, category, image }) => {
 				</div>
 			)}
 
-			<button className='mt-auto button'>Add To Basket</button>
+			<button onClick={addItemToBasket} className='mt-auto button'>
+				Add To Basket
+			</button>
 		</div>
 	)
 }
